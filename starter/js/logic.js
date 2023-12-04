ef1a560592b10e385564901eed904d64fac2f6ad
tosin1691 <119135079+tosin1691@users.noreply.github.com>
2023-12-04 13:29:59 +0000

    added code to JS to enable users to start quiz and for questions to pop up.

--- starter/js/logic.js
+++ starter/js/logic.js
@@ -1,1 +1,37 @@
 
+let startBtn = document.querySelector("#start")
+let startScreenEl = document.querySelector("#start-screen")
+let questionsEl = document.querySelector("#questions")
+let questionTitleEl = document.querySelector("#question-title")
+let choicesEl = document.querySelector("#choices")
+
+// const choicesListEl = document.createElement("ol")
+// const choicesLi1= document.createElement("li")
+// const choicesLi2= document.createElement("li")
+// const choicesLi3= document.createElement("li")
+// const choicesLi4= document.createElement("li")
+
+let randomisedQuestions
+let currentQuestionIndex
+
+
+function startQuiz(){
+    startScreenEl.classList.add('hide')
+    questionsEl.classList.remove('hide')
+    randomisedQuestions = questions.sort(function(a, b){
+        return 0.5 - Math.random()
+      })
+    currentQuestionIndex = 0
+    showQuestions(randomisedQuestions[currentQuestionIndex])
+
+
+}
+
+function showQuestions(question) {
+    questionTitleEl.textContent = question.question
+    
+    }
+
+
+startBtn.addEventListener('click', startQuiz)
+
--- starter/js/questions.js
+++ starter/js/questions.js
@@ -2,7 +2,7 @@
 
 const questions = [
     {
-        questions: "Which attribute must have a unique value each time it is used in an HTML document?",
+        question: "Which attribute must have a unique value each time it is used in an HTML document?",
         answers: [
             {text: "id", correct: true},
             {text: "title", correct: false},
@@ -12,7 +12,7 @@ const questions = [
     },
 
     {
-        questions: "What is the correct way to initialize an array of planets in JavaScript?",
+        question: "What is the correct way to initialize an array of planets in JavaScript?",
         answers: [
             {text: 'let planets = {Venus, Mars, Earth}', correct: false},
             {text: 'String[] planets = new Array("Venus", "Mars", "Earth");', correct: false},
@@ -22,7 +22,7 @@ const questions = [
     },
     
     {
-        questions: "The browser finds some CSS that it does not understand. What is likely happen?",
+        question: "The browser finds some CSS that it does not understand. What is likely happen?",
         answers: [
             {text: "The page will not display", correct: false},
             {text: "An error message will be displayed", correct: false},
@@ -32,7 +32,7 @@ const questions = [
     },
 
     {
-        questions: "What does the === comparison operator do?",
+        question: "What does the === comparison operator do?",
         answers: [
             {text: "It tests for equality of value only", correct: false},
             {text: "It tests for equality of value and type", correct: true},
@@ -42,7 +42,7 @@ const questions = [
     },
 
     {
-        questions: "Commonly used data type DO NOT include",
+        question: "Commonly used data type DO NOT include",
         answers: [
             {text: "strings", correct: false},
             {text: "booleans", correct: false},
@@ -52,7 +52,7 @@ const questions = [
     },
 
     {
-        questions: "The condition in an if / else statement is enclosed within ______.",
+        question: "The condition in an if / else statement is enclosed within ______.",
         answers: [
             {text: "quotes", correct: false},
             {text: "curly brackets", correct: false},
