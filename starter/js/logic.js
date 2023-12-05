33687e6827a836d83502f42470ddca9f6e5c0513
tosin1691 <119135079+tosin1691@users.noreply.github.com>
2023-12-05 01:08:18 +0000

    added more lines of code to JS to enable users to start and end the quiz

--- starter/js/logic.js
+++ starter/js/logic.js
@@ -1,36 +1,130 @@
 
-let startBtn = document.querySelector("#start")
-let startScreenEl = document.querySelector("#start-screen")
-let questionsEl = document.querySelector("#questions")
-let questionTitleEl = document.querySelector("#question-title")
-let choicesEl = document.querySelector("#choices")
-
-// const choicesListEl = document.createElement("ol")
-// const choicesLi1= document.createElement("li")
-// const choicesLi2= document.createElement("li")
-// const choicesLi3= document.createElement("li")
-// const choicesLi4= document.createElement("li")
+const startBtn = document.querySelector("#start")
+const startScreenEl = document.querySelector("#start-screen")
+const questionsEl = document.querySelector("#questions")
+const questionTitleEl = document.querySelector("#question-title")
+const choicesEl = document.querySelector("#choices")
+const timeEl = document.querySelector("#time")
+const timerEl = document.querySelector(".timer")
+const correctAnswerEl = document.querySelector("#correct-answer")
+const lineBreakEl = document.querySelector("#line-break")
+const endScreenEl = document.querySelector("#end-screen")
 
 let randomisedQuestions
-let currentQuestionIndex
+let currentQuestionIndex = 0
+let choices
+let score = 0
+let secondsLeft = 75
 
 
 function startQuiz(){
+    
     startScreenEl.classList.add('hide')
     questionsEl.classList.remove('hide')
+    timerEl.classList.remove('hide')
     randomisedQuestions = questions.sort(function(a, b){
         return 0.5 - Math.random()
       })
     currentQuestionIndex = 0
     showQuestions(randomisedQuestions[currentQuestionIndex])
 
+    quizTimer()
 
 }
 
 function showQuestions(question) {
     questionTitleEl.textContent = question.question
     
+    if (!lineBreakEl.className.includes("hide")){
+        lineBreakEl.classList.add("hide")
     }
+
+    if (!correctAnswerEl.className.includes("hide")){
+        correctAnswerEl.classList.add("hide")
+    }
+
+    Array.from(choicesEl.childNodes.entries()).map(function([index, child]){
+        choicesEl.removeChild(child)
+    })
+
+    question.answers.map(function(answer){
+
+        choices = document.createElement("button")
+        choices.textContent = answer.text
+        choicesEl.appendChild(choices)
+
+
+        if(answer.correct){
+            choices.classList.add("correct")
+        } else {
+            choices.classList.add("incorrect")
+        }
+
+        choices.addEventListener("click", selectedAnswer)
+    })
+   
+    }
+
+    function selectedAnswer(event){
+
+        event.preventDefault()
+            
+        let selectedAnswer = event.target
+            
+            if(selectedAnswer.matches(".correct")) {
+                correctAnswerEl.classList.remove("hide")
+                lineBreakEl.classList.remove("hide")
+                correctAnswerEl.textContent = "Correct!"
+                score++
+            } else if (selectedAnswer.matches(".incorrect")) {
+                correctAnswerEl.classList.remove("hide")
+                lineBreakEl.classList.remove("hide")
+                correctAnswerEl.textContent = "Wrong!"
+                secondsLeft = secondsLeft - 5
+                timeEl.textContent = secondsLeft
+            }
+
+            
+                setTimeout(function(){
+                    nextQuestion()
+                    
+                }, 300)
+            
+        }
+
+    function quizTimer(){
+        let timeInterval = setInterval(function(){
+        secondsLeft--
+        timeEl.textContent = secondsLeft
+
+        if (secondsLeft <= 0) {
+            clearInterval(timeInterval)
+            endQuiz()
+        }
+        }, 1000)
+        
+    }
+
+    function nextQuestion(){
+        currentQuestionIndex++
+
+        if(randomisedQuestions.length === currentQuestionIndex) {
+            endQuiz()
+
+        } else {
+        showQuestions(randomisedQuestions[currentQuestionIndex])
+        }
+    
+    }
+
+    function endQuiz(){
+        questionsEl.classList.add('hide')
+        endScreenEl.classList.remove('hide')
+        timerEl.classList.add('hide')
+    }
+
+    
+
 
 
 startBtn.addEventListener('click', startQuiz)
--- starter/js/questions.js
+++ starter/js/questions.js
@@ -4,60 +4,60 @@ const questions = [
     {
         question: "Which attribute must have a unique value each time it is used in an HTML document?",
         answers: [
-            {text: "id", correct: true},
-            {text: "title", correct: false},
-            {text: "class", correct: false},
-            {text: "style", correct: false},
+            {text: "1. id", correct: true},
+            {text: "2. title", correct: false},
+            {text: "3. class", correct: false},
+            {text: "4. style", correct: false},
         ]
     },
 
     {
         question: "What is the correct way to initialize an array of planets in JavaScript?",
         answers: [
-            {text: 'let planets = {Venus, Mars, Earth}', correct: false},
-            {text: 'String[] planets = new Array("Venus", "Mars", "Earth");', correct: false},
-            {text: 'let planets = ["Venus", "Mars", "Earth"];', correct: true},
-            {text: 'let galaxies = {"Venus", "Mars", "Earth"}', correct: false},
+            {text: '1. let planets = {Venus, Mars, Earth}', correct: false},
+            {text: '2. String[] planets = new Array("Venus", "Mars", "Earth");', correct: false},
+            {text: '3. let planets = ["Venus", "Mars", "Earth"];', correct: true},
+            {text: '4. let galaxies = {"Venus", "Mars", "Earth"}', correct: false},
         ]
     },
     
     {
         question: "The browser finds some CSS that it does not understand. What is likely happen?",
         answers: [
-            {text: "The page will not display", correct: false},
-            {text: "An error message will be displayed", correct: false},
-            {text: "The browser will not load the stylesheet", correct: false},
-            {text: "The browser will ignore the unknown CSS", correct: true},
+            {text: "1. The page will not display", correct: false},
+            {text: "2. An error message will be displayed", correct: false},
+            {text: "3. The browser will not load the stylesheet", correct: false},
+            {text: "4. The browser will ignore the unknown CSS", correct: true},
         ]
     },
 
     {
         question: "What does the === comparison operator do?",
         answers: [
-            {text: "It tests for equality of value only", correct: false},
-            {text: "It tests for equality of value and type", correct: true},
-            {text: "It sets one variable equal to another in both value and type", correct: false},
-            {text: "It tests for equality of type only", correct: false},
+            {text: "1. It tests for equality of value only", correct: false},
+            {text: "2. It tests for equality of value and type", correct: true},
+            {text: "3. It sets one variable equal to another in both value and type", correct: false},
+            {text: "4. It tests for equality of type only", correct: false},
         ]
     },
 
     {
         question: "Commonly used data type DO NOT include",
         answers: [
-            {text: "strings", correct: false},
-            {text: "booleans", correct: false},
-            {text: "alerts", correct: true},
-            {text: "numbers", correct: false},
+            {text: "1. strings", correct: false},
+            {text: "2. booleans", correct: false},
+            {text: "3. alerts", correct: true},
+            {text: "4. numbers", correct: false},
         ]
     },
 
     {
         question: "The condition in an if / else statement is enclosed within ______.",
         answers: [
-            {text: "quotes", correct: false},
-            {text: "curly brackets", correct: false},
-            {text: "parentheses", correct: true},
-            {text: "square brackets", correct: false},
+            {text: "1. quotes", correct: false},
+            {text: "2. curly brackets", correct: false},
+            {text: "3. parentheses", correct: true},
+            {text: "4. square brackets", correct: false},
         ]
     }
 ]
